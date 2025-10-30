74fa080b698d83499c094cab35f9e33d49da135a
100ghxck <100ghxck@proton.me>
2025-10-30 08:04:44 +0100

    klasse gelöscht

--- charaktere.gd
+++ charaktere.gd
@@ -2,25 +2,43 @@ extends Node
 class_name character
 
 var hp: float
-var attack: Callable
+var damadge: float
+var poison: bool
+var Name: String
+#var attack: Callable
 #prüfen, ob attacke ok ist
-func _init(func_ref: Callable, HP: float) ->void:
+func _init(_hp: float, _damadge:float, _Name:String) ->void:
+	hp = _hp
+	damadge = _damadge
+	Name = _Name
 	#ist func_ref ein Callable?
-	assert(func_ref is Callable, "übergebener Parameter ist kein Callable")
+	#assert(func_ref is Callable, "übergebener Parameter ist kein Callable")
 	
 	#existiert func_ref?
-	assert(func_ref.is_valid, "Callable ist ungültig, verweist auf nicht existente Methode")
+	#assert(func_ref.is_valid, "Callable ist ungültig, verweist auf nicht existente Methode")
 	
 	#ist func_ref func_ref in attacks
-	var obj = func_ref.get_object()
-	assert(obj is attacks, "Callable keine Funktion von attacks")
+	#var obj = func_ref.get_object()
+	#assert(obj is attacks, "Callable keine Funktion von attacks")
 	
 	#ist HP float?
-	assert(HP is float, "HP muss float sein")
+	#assert(HP is float, "HP muss float sein")
 	
 	#alles stimmt
-	attack = func_ref
-	print("charakter erfolgreich erstellt")
+	#attack = func_ref
+	#print("charakter erfolgreich erstellt")
+func attack(target: character)->void:
+	if target == self:
+		print("cannot attack yourself")
+		pass
+	else:
+		print(self.Name, " is attacking ", target.Name, " for ", self.damadge, " damadge")
+		target.hp -= damadge
+	
+	
+	
+	
+	
 	
 	
 	
--- node.gd
+++ node.gd
@@ -1,15 +1,52 @@
 extends Node
+
+signal keypress
+
+#funktionen
 func p1_turn()->void:
-	print("spieler 1 zieht")
+	if fighting ==true:
+		print("spieler 1 zieht")
+		await keypress
+		p1.attack(p2)
+		print(p2.Name, " has ", p2.hp, " hp left ")
+		playerswitch()	
 func p2_turn()->void:
-	print("spieler 2 zieht")
-var event_p1_turn: Callable = p1_turn
-var event_p2_turn: Callable = p2_turn
+	if fighting == true:
+		print("spieler 2 zieht")
+		await keypress
+		p2.attack(p1)
+		print(p1.Name, " has ", p1.hp, " hp left ")
+		playerswitch()
+#variablen
 var game_sequence: Array = [p1_turn, p2_turn]
+#var attacks_beispiel = attacks.new()
+var monkey = character.new(100, 30, "monkey")
+var girrafe = character.new(150, 20, "girrafe")
+var p1 = monkey
+var p2 = girrafe
+var fighting: bool = true
+var current_event: int = 0 
 func _ready() ->void:
-	var attacks_beispiel = attacks.new()
-	var icon = character.new( Callable(attacks_beispiel, "hit"),float(2.3))
+	playerswitch()
+	#var attacks_beispiel = attacks.new()
+	#var p1 = character.new()
+	
+	
 func _process(delta) ->void:
-	for current_event in game_sequence:
-		current_event.call
+	if p1.hp <= 0:
+		fighting = false
+	#game_sequence[current_event].call()
+	#current_event += 1
+	#if current_event > len(game_sequence)-1:
+		#current_event = 0
 		
+func _input(_i):
+	if Input.is_action_just_pressed("first attack"):
+		keypress.emit()
+
+func playerswitch()->void:
+	game_sequence[current_event].call()
+	current_event += 1
+	if current_event > len(game_sequence)-1:
+		current_event = 0
+	
