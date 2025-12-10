0c0523f139c7cf93ccb0ba0d84bf9647a53c38f1
100ghxck <100ghxck@proton.me>
2025-12-10 18:10:06 +0100

    error

--- ui.gd
+++ ui.gd
@@ -18,11 +18,14 @@ func _ready():
 		if Character.p1 and Character and $"../player container/player 1" and $"../player container/player 2" and $"../player container/player 3" and $"../enemy container/enemy 1 area/AnimatedSprite2D" and $"../enemy container/enemy 2 area/AnimatedSprite2D" and $"../enemy container/enemy 3 area/AnimatedSprite2D":
 			break
 		await get_tree().process_frame
-	
-	p1_textures = [Character.p1[0].texture, Character.p1[1].texture, Character.p1[2].texture]
-	p2_textures = [Character.p2[0].texture, Character.p2[1].texture, Character.p2[2].texture]
 	p1_sprites = [$"../player container/player 1".sprite_frames, $"../player container/player 2".sprite_frames, $"../player container/player 3".sprite_frames]
 	p2_sprites = [$"../enemy container/enemy 1 area/AnimatedSprite2D".sprite_frames, $"../enemy container/enemy 2 area/AnimatedSprite2D".sprite_frames, $"../enemy container/enemy 3 area/AnimatedSprite2D".sprite_frames]
+	for x in range(3):
+		print(get_node("../player container/player %d/AnimatedSprite2D" % (x+1)).sprite_frames, Character.p1[x].texture)
+		get_node("../player container/player %d/AnimatedSprite2D" % (x+1)).sprite_frames = Character.p1[x].texture
+	for x in range(3):
+		print(get_node("../enemy container/enemy %d area/AnimatedSprite2D" % (x+1)).sprite_frames, Character.p2[x].texture)
+		get_node("../enemy container/enemy %d area/AnimatedSprite2D" % (x+1)).sprite_frames = Character.p2[x].texture
 func _process(delta)->void:
 	text1.text = "attack 1"
 	text2.text = "attack 2"
@@ -30,8 +33,8 @@ func _process(delta)->void:
 	if $"..".current_attacker:
 		current_attacker = $"..".current_attacker
 		elements = [current_attacker.n_attack1_element,
-	 	current_attacker.n_attack2_element,
-	 	current_attacker.n_attack3_element]
+		current_attacker.n_attack2_element,
+		current_attacker.n_attack3_element]
 		for i in range(0,3):
 			var icon = icons[i]
 			var element = elements[i]
@@ -44,11 +47,7 @@ func _process(delta)->void:
 				"wind" : x = 4
 				"plant" : x = 5
 			icon.frame = x
-		
-		
-
-
-func _on_kampf_turn_switch() -> void:
-	for x in range(0,3):
-		print(p1_sprites[x], p1_textures[x])
-		p1_sprites[x] = p1_textures[x] 
+	if $"../enemy container/enemy 1 area/enemy 1/HP 1" and $"../enemy container/enemy 2 area/enemy 2/HP 2" and $"../enemy container/enemy 3 area/enemy 3/HP 3":
+		$"../enemy container/enemy 1 area/enemy 1/HP 1".text = var_to_str(roundf(Character.p2[0].hp))
+		$"../enemy container/enemy 2 area/enemy 2/HP 2".text = var_to_str(round(Character.p2[1].hp))
+		$"../enemy container/enemy 3 area/enemy 3/HP 3".text = var_to_str(round(Character.p2[2].hp))
